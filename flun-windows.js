448ed4fc8930cb9237d1a22e47871e7a669f2882
flunGit <cn@flun.top>
2026-02-02 11:44:27 +0800

    renamed:    lib/flun-windows.js -> flun-windows.js 	modified:   package.json

--- flun-windows.js
+++ flun-windows.js
@@ -3,8 +3,8 @@
  * @description 提供Windows特定功能，如提升权限、事件日志记录和服务管理
  * @author Corey Butler
  */
-const os = require('os'), { elevate, sudo, isAdminUser } = require('./binaries'), { kill, list } = require('./cmd'),
-  Service = require('./daemon'), EventLogger = require('./eventlog');
+const os = require('os'), { elevate, sudo, isAdminUser } = require('./lib/binaries'), { kill, list } = require('./lib/cmd'),
+  Service = require('./lib/daemon'), EventLogger = require('./lib/eventlog');
 
 // 平台检查
 if (!os.platform().startsWith('win')) throw new Error('flun-windows 仅支持在Windows系统上运行');
